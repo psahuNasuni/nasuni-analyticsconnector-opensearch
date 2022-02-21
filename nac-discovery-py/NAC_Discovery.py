f69e0f25b9e8346021c4d732bf03579e9f9196a3
sumeetdeveloper <rvkarsumeet@gmail.com>
2022-02-21 09:23:00 +0530

    Add code for root handle special characters fix

--- nac-discovery-py/NAC_Discovery.py
+++ nac-discovery-py/NAC_Discovery.py
@@ -67,7 +67,8 @@ def lambda_handler(event, context):
         data['extension'] = file_name[file_name.index('.')+1:]
         data['volume_name'] = secret_data_internal['volume_name']
         
-        data['root_handle'] = secret_data_internal['root_handle'].replace('.','_').lower()
+        #data['root_handle'] = secret_data_internal['root_handle'].replace('.','_').lower()
+        data['root_handle'] = re.sub('[!@#$%^&*()+?=,<>/.]', '-', secret_data_internal['root_handle']).lower()
         data['source_bucket'] = secret_data_internal['discovery_source_bucket']
         print("data['object_key']",data['object_key'])  
         print("data['dest_bucket']",data['dest_bucket'])  
