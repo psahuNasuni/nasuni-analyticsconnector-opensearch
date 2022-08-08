bc1aedd0162b4542a6fe27f6287df0df1876d685
sumeetpynasuni <87188051+sumeetpynasuni@users.noreply.github.com>
2022-08-08 04:48:52 -0700

    Update variables.tf
    python3.9 to python3.8

--- variables.tf
+++ variables.tf
@@ -115,7 +115,7 @@ variable "nac_destination_bucket" {
   description = "S3 bucket where NAC will be updating the files/data"
 }
 variable "runtime" {
-  default = "python3.9"
+  default = "python3.8"
 }
 
 variable "aws_profile" {
